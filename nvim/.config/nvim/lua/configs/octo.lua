a03830a93016ce181e0247c1a063d4e135fdf57f
Trym Veiby <tve@variant.no>
2026-03-19 11:18:55 +0100

    fix(nvim): octo break lines with random code

--- nvim/.config/nvim/lua/configs/octo.lua
+++ nvim/.config/nvim/lua/configs/octo.lua
@@ -1,11 +1,19 @@
 require("octo").setup {
   enable_builtin = true,
   default_remote = { "upstream", "origin" },
-  picker = "telescope",
+  picker = "snacks",
   picker_config = {
-    use_emojis = false,
+    use_emojis = true,
   },
   suppress_missing_scope = {
     projects_v2 = true,
   },
 }
+
+-- Clean up fold display in Octo diff buffers
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "octo://*",
+  callback = function()
+    vim.opt_local.foldtext = ""
+  end,
+})
