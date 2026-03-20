49ad42ec6021842b373446840849b7667abc43a2
Trym Veiby <tve@variant.no>
2026-03-20 08:34:06 +0100

    feat(nvim): add keybind for diffview file history

--- nvim/.config/nvim/lua/mapping/diffview.lua
+++ nvim/.config/nvim/lua/mapping/diffview.lua
@@ -2,4 +2,5 @@ return {
   vim.keymap.set("n", "<leader>gdo", "<cmd>DiffviewOpen<CR>", { desc = "Open Diffview" }),
   vim.keymap.set("n", "<leader>gdc", "<cmd>DiffviewClose<CR>", { desc = "Close Diffview" }),
   vim.keymap.set("n", "<leader>gdr", "<cmd>DiffviewRefresh<CR>", { desc = "Refresh Diffview" }),
+  vim.keymap.set("n", "<leader>gdh", "<cmd>DiffviewFileHistory<CR>", { desc = "Refresh Diffview" }),
 }
